e702d1fe9886794282ffa88ef75686504f405f54
bjlkak <bjlkak@hotmail.com>
2020-05-15 15:07:17 -0700

    fun

--- src/May.java
+++ src/May.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -29,4 +30,58 @@ public class May {
         }
         return i;
     }
+
+    class Trie {
+
+        /** Initialize your data structure here. */
+        public boolean end;
+        public HashMap<Character,Trie> map;
+        public Trie() {
+            end = false;
+            map = new HashMap<>();
+        }
+
+        /** Inserts a word into the trie. */
+        public void insert(String word) {
+            Trie current = this;
+            for(int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+                if(current.map.containsKey(c)) {
+                    current = current.map.get(c);
+                }
+                else {
+                    current.map.put(c, new Trie());
+                    current = current.map.get(c);
+                }
+                if(i == word.length() - 1) {
+                    current.end = true;
+                }
+            }
+        }
+
+        /** Returns if the word is in the trie. */
+        public boolean search(String word) {
+            Trie current = this;
+            for(char c : word.toCharArray()) {
+                if(!current.map.containsKey(c)){
+                    return false;
+                }
+                current = current.map.get(c);
+            }
+            return current.end;
+        }
+
+        /** Returns if there is any word in the trie that starts with the given prefix. */
+        public boolean startsWith(String prefix) {
+            Trie current = this;
+            for(char c : prefix.toCharArray()) {
+                if(!current.map.containsKey(c)){
+                    return false;
+                }
+                current = current.map.get(c);
+            }
+            return true;
+        }
+    }
+
 }
